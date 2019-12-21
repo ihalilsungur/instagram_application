babd6bbdc314b9081e0930110e656ac14083cf32
ihalilsungur <ihalilsungur@gmail.com>
2019-12-21 12:57:51 +0300

    projemizde bulunan homeScreen sınıfımızda güncellemler yapıldı

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_application/screens/activity_screen.dart';
+import 'package:instagram_application/screens/create_post_screen.dart';
+import 'package:instagram_application/screens/feed_screen.dart';
+import 'package:instagram_application/screens/profile_screen.dart';
+import 'package:instagram_application/screens/search_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  final String userId;
+  HomeScreen({this.userId});
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentTab = 0;
+  PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Text(
+            "Instagram",
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: "Billabong",
+                fontSize: 35,
+                fontWeight: FontWeight.w300),
+          ),
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          FeedScreen(),
+          SearchScreen(),
+          CreatePostScreen(),
+          ActivityScreen(),
+          ProfileScreen(userId: widget.userId,)
+        ],
+        onPageChanged: (int index) {
+          setState(() {
+            _currentTab = index;
+          });
+        },
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        currentIndex: _currentTab,
+        onTap: (int index) {
+          setState(() {
+            _currentTab = index;
+          });
+          _pageController.animateToPage(index,
+              duration: Duration(milliseconds: 200), curve: Curves.easeIn);
+        },
+        activeColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 35,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              size: 35,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.photo_camera,
+              size: 35,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notifications,
+              size: 35,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.account_circle,
+              size: 35,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
