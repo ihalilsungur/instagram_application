3103a908cf1470e08656799eb42120559138c19f
ihalilsungur <ihalilsungur@gmail.com>
2019-12-23 11:57:14 +0300

    CreatePost_screen sınıfı eklendi

--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -9,6 +9,19 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Text(
+            "Instagram",
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: "Billabong",
+                fontSize: 35,
+                fontWeight: FontWeight.w300),
+          ),
+        ),
+      ),
       body: Center(
       child: Text("Create Post Screen"),
       ),
