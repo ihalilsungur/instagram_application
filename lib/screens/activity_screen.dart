3454ea95ad71ff435faa06a87e742b4c07682e5b
ihalilsungur <ihalilsungur@gmail.com>
2019-12-23 11:56:29 +0300

    Projeye activityScreen sınıfı eklendi

--- lib/screens/activity_screen.dart
+++ lib/screens/activity_screen.dart
@@ -9,6 +9,19 @@ class _ActivityScreenState extends State<ActivityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Text(
+            "Instagram",
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: "Billabong",
+                fontSize: 35,
+                fontWeight: FontWeight.w300),
+          ),
+        ),
+      ),
       body: Center(child: Text("Activity Screen")),
     );
   }
