cc14a150de5080d92e8ee21ccb37d854b1fd0a19
ihalilsungur <ihalilsungur@gmail.com>
2019-12-23 11:02:27 +0300

    projeyenin modele User sınıfı eklendi

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,14 +3,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class User {
   final String id;
   final String name;
-  final String profilrImageUrl;
+  final String profileImageUrl;
   final String email;
   final String bio;
 
   User({
     this.id,
     this.name,
-    this.profilrImageUrl,
+    this.profileImageUrl,
     this.email,
     this.bio,
   });
@@ -19,7 +19,7 @@ class User {
     return User(
       id: doc.documentID,
       name : doc["name"],
-      profilrImageUrl: doc["profileImageUrl"],
+      profileImageUrl: doc["profileImageUrl"],
       email: doc["email"],
       bio: doc["bio"] ?? ""
     );
