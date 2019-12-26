840e50ee20735e85a0034623bd4776c54e1a4b0e
ihalilsungur <ihalilsungur@gmail.com>
2019-12-26 17:42:12 +0300

    Projemizde bulunan constants dökümantasyonu postRef , followersRef ve followingRef değişkenlikleri eklendi

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -4,3 +4,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 final _firestore = Firestore.instance;
 final storageRef = FirebaseStorage.instance.ref();
 final userRef = _firestore.collection("users");
+final postRef = _firestore.collection("posts");
+final followersRef = _firestore.collection("followers");
+final followingRef = _firestore.collection("following");
