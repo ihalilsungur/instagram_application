95d8aded9d77d534b44e15807115ec66cf68c6a3
ihalilsungur <ihalilsungur@gmail.com>
2019-12-26 17:27:52 +0300

    projemizin main.dart sınıfı güncellendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_application/models/user.data.dart';
 import 'package:instagram_application/screens/feed_screen.dart';
 import 'package:instagram_application/screens/home_screen.dart';
 import 'package:instagram_application/screens/login_screen.dart';
 import 'package:instagram_application/screens/signup_screen.dart';
+import 'package:provider/provider.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -11,10 +15,10 @@ class MyApp extends StatelessWidget {
   Widget _getScreenId() {
     return StreamBuilder<FirebaseUser>(
       stream: FirebaseAuth.instance.onAuthStateChanged,
-      builder: (BuildContext context, shapshot) {
-        if (shapshot.hasData) {
+      builder: (BuildContext context, snapshot) {
+        if (snapshot.hasData) {
+          Provider.of<UserData>(context).currentUserId = snapshot.data.uid;
           return HomeScreen(
-            userId: shapshot.data.uid,
           );
         } else {
           return LoginScreen();
@@ -25,20 +29,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram Application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryIconTheme:
-            Theme.of(context).primaryIconTheme.copyWith(color: Colors.black),
+    return ChangeNotifierProvider(
+      create: (context) => UserData(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Application',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          primaryIconTheme:
+              Theme.of(context).primaryIconTheme.copyWith(color: Colors.black),
+        ),
+        home: _getScreenId(),
+        routes: {
+          LoginScreen.id: (context) => LoginScreen(),
+          SignUpScreen.id: (context) => SignUpScreen(),
+          FeedScreen.id: (context) => FeedScreen()
+        },
       ),
-      home: _getScreenId(),
-      routes: {
-        LoginScreen.id: (context) => LoginScreen(),
-        SignUpScreen.id: (context) => SignUpScreen(),
-        FeedScreen.id: (context) => FeedScreen()
-      },
     );
   }
 }
