b0442453447e1e23d0193ce4c87f5978447abb24
ihalilsungur <ihalilsungur@gmail.com>
2019-12-27 17:29:28 +0300

    projemizde bulunan ProfileScreen Sınıfına _buildTilePost ve _buildDisplayPosts metodları eklendi

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_application/models/post.dart';
 import 'package:instagram_application/models/user.dart';
 import 'package:instagram_application/models/user.data.dart';
 import 'package:instagram_application/screens/edit_profile_screen.dart';
 import 'package:instagram_application/services/database_service.dart';
 import 'package:instagram_application/utilities/constants.dart';
+import 'package:instagram_application/widgets/post_view.dart';
 import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -17,9 +19,12 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  bool isFollowing = false;
-  int followerCount = 0;
-  int followingCount = 0;
+  bool _isFollowing = false;
+  int _followerCount = 0;
+  int _followingCount = 0;
+  List<Post> _posts = [];
+  int _displayPosts = 0; //0-grid 1-column
+  User _profileUser;
 
   @override
   void initState() {
@@ -27,62 +32,75 @@ class _ProfileScreenState extends State<ProfileScreen> {
     _setupIsFollowing();
     _setupFollowers();
     _setupFollowing();
+    _setupPosts();
+    _setupProfileUser();
   }
 
   _setupIsFollowing() async {
     bool _isFollowingUser = await DatabaseService.isFollowing(
         currentUserId: widget.currentUserId, userId: widget.userId);
     setState(() {
-      isFollowing = _isFollowingUser;
+      _isFollowing = _isFollowingUser;
     });
   }
 
   _setupFollowers() async {
     int _userFollowerCount = await DatabaseService.numFollowers(widget.userId);
     setState(() {
-      followerCount = _userFollowerCount;
+      _followerCount = _userFollowerCount;
     });
   }
 
   _setupFollowing() async {
     int _userFollowingCount = await DatabaseService.numFollowing(widget.userId);
     setState(() {
-      followingCount = _userFollowingCount;
+      _followingCount = _userFollowingCount;
     });
   }
 
- _followOrUnfollow(){
-   if (isFollowing) {
-     _unfollowUser();
-   }else{
-     _followUser();
-   }
- }
+  _setupPosts() async {
+    List<Post> posts = await DatabaseService.getUserPosts(widget.userId);
+    setState(() {
+      _posts = posts;
+    });
+  }
 
- _followUser(){
-   DatabaseService.followUser(
-     currentUserId: widget.currentUserId,
-     userId: widget.userId
-   );
-   setState(() {
-     isFollowing = true;
-     followerCount++;
-   });
+  _setupProfileUser() async {
+    User profileUser = await DatabaseService.getUserWithId(widget.userId);
+    setState(() {
+      _profileUser = profileUser;
+    });
+  }
 
-   print("_followUser() metodu _isFollow :"+isFollowing.toString());
- }
+  _followOrUnfollow() {
+    if (_isFollowing) {
+      _unfollowUser();
+    } else {
+      _followUser();
+    }
+  }
 
- _unfollowUser(){
-   DatabaseService.unFollowUser(
-     currentUserId: widget.currentUserId,
-     userId: widget.userId
-   );
-   setState(() {
-     isFollowing = false;
-     followerCount--;
-   });
-   print("_unfollowUser() metodu _isFollow :"+isFollowing.toString());
- }
+  _followUser() {
+    DatabaseService.followUser(
+        currentUserId: widget.currentUserId, userId: widget.userId);
+    setState(() {
+      _isFollowing = true;
+      _followerCount++;
+    });
+
+    print("_followUser() metodu _isFollow :" + _isFollowing.toString());
+  }
+
+  _unfollowUser() {
+    DatabaseService.unFollowUser(
+        currentUserId: widget.currentUserId, userId: widget.userId);
+    setState(() {
+      _isFollowing = false;
+      _followerCount--;
+    });
+    print("_unfollowUser() metodu _isFollow :" + _isFollowing.toString());
+  }
+
   _displayButton(User _user) {
     return _user.id == Provider.of<UserData>(context).currentUserId
         ? Container(
@@ -109,15 +127,183 @@ class _ProfileScreenState extends State<ProfileScreen> {
         : Container(
             width: MediaQuery.of(context).size.width * 1 / 2 - 10,
             child: RaisedButton(
-              color: isFollowing ? Colors.grey[200] : Colors.blue,
-              textColor: isFollowing ? Colors.black : Colors.white,
-              onPressed: () =>_followOrUnfollow(),
+              color: _isFollowing ? Colors.grey[200] : Colors.blue,
+              textColor: _isFollowing ? Colors.black : Colors.white,
+              onPressed: () => _followOrUnfollow(),
               child: Text(
-                isFollowing ? "Vazgeç" : "Takip Et",
+                _isFollowing ? "Vazgeç" : "Takip Et",
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
               ),
             ),
           );
+  }
+
+  _buildProfileInfo(User _user) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+          child: Row(
+            children: <Widget>[
+              CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.grey,
+                  backgroundImage: _user.profileImageUrl.isEmpty
+                      ? AssetImage("assets/images/user_placeholder.jpg")
+                      : CachedNetworkImageProvider(_user.profileImageUrl)),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              _posts.length.toString(),
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey),
+                            ),
+                            Text("Gönderi")
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              _followerCount.toString(),
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey),
+                            ),
+                            Text(
+                              "Takipçi",
+                              style: TextStyle(
+                                color: Colors.black87,
+                              ),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              _followingCount.toString(),
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey),
+                            ),
+                            Text("Takip")
+                          ],
+                        ),
+                      ],
+                    ),
+                    _displayButton(_user),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                _user.name,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 5),
+              Container(
+                height: 100,
+                child: Text(
+                  _user.bio,
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              Divider(
+                height: 1,
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  _buildToggleButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.grid_on,
+            size: 30,
+          ),
+          color: _displayPosts == 0
+              ? Theme.of(context).primaryColor
+              : Colors.grey[300],
+          onPressed: () => setState(() {
+            _displayPosts = 0;
+          }),
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.list,
+            size: 30,
+          ),
+          color: _displayPosts == 1
+              ? Theme.of(context).primaryColor
+              : Colors.grey[300],
+          onPressed: () => setState(() {
+            _displayPosts = 1;
+          }),
+        ),
+      ],
+    );
+  }
+
+  _buildTilePost(Post post) {
+    return GridTile(
+      child: Image(
+        image: CachedNetworkImageProvider(post.imageUrl),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  _buildDisplayPosts() {
+    if (_displayPosts == 0) {
+      //Grid
+      List<GridTile> tiles = [];
+      _posts.forEach((post) => tiles.add(_buildTilePost(post)));
+      return GridView.count(
+        crossAxisCount: 3,
+        childAspectRatio: 1,
+        mainAxisSpacing: 2,
+        crossAxisSpacing: 2,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        children: tiles,
+
+      );
+    } else {
+      //Column
+      List<PostView> postViews = [];
+      _posts.forEach((post) {
+        postViews.add(PostView(
+          currentUserId: widget.currentUserId,
+          post: post,
+          author: _profileUser,
+        ));
+      });
+      return Column(
+        children: postViews,
+      );
+    }
   }
 
   @override
@@ -149,95 +335,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
           return ListView(
             children: <Widget>[
-              Padding(
-                padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
-                child: Row(
-                  children: <Widget>[
-                    CircleAvatar(
-                        radius: 50,
-                        backgroundColor: Colors.grey,
-                        backgroundImage: _user.profileImageUrl.isEmpty
-                            ? AssetImage("assets/images/user_placeholder.jpg")
-                            : CachedNetworkImageProvider(
-                                _user.profileImageUrl)),
-                    Expanded(
-                      child: Column(
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Column(
-                                children: <Widget>[
-                                  Text(
-                                    "12",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.grey),
-                                  ),
-                                  Text("Gönderi")
-                                ],
-                              ),
-                              Column(
-                                children: <Widget>[
-                                  Text(
-                                    followerCount.toString(),
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.grey),
-                                  ),
-                                  Text("Takipçi",style: TextStyle(
-                                    color: Colors.black87,
-                                   
-                                  ),)
-                                ],
-                              ),
-                              Column(
-                                children: <Widget>[
-                                  Text(
-                                    followingCount.toString(),
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.grey),
-                                  ),
-                                  Text("Takip")
-                                ],
-                              ),
-                            ],
-                          ),
-                          _displayButton(_user),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      _user.name,
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 5),
-                    Container(
-                      height: 100,
-                      child: Text(
-                        _user.bio,
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ),
-                    Divider(
-                      height: 1,
-                    )
-                  ],
-                ),
-              ),
+              _buildProfileInfo(_user),
+              _buildToggleButtons(),
+              Divider(),
+              _buildDisplayPosts(),
             ],
           );
         },
