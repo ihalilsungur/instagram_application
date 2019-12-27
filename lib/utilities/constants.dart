37d847dbd315d0e66715b8cc06201a352e23831c
ihalilsungur <ihalilsungur@gmail.com>
2019-12-27 17:26:20 +0300

    projemizde bulunan constants.dart dökümanı güncellendi

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -4,7 +4,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 final _firestore = Firestore.instance;
 final storageRef = FirebaseStorage.instance.ref();
 final userRef = _firestore.collection("users");
-final postRef = _firestore.collection("posts");
+final postsRef = _firestore.collection("posts");
 final followersRef = _firestore.collection("followers");
 final followingRef = _firestore.collection("following");
 final feedsRef = _firestore.collection("feeds");
