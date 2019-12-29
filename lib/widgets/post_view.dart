38a6a2f0ea008501196d271b1466864e1b593ece
ihalilsungur <ihalilsungur@gmail.com>
2019-12-29 13:15:42 +0300

    PostView sınıfında bazı güncellemler yapıldı

--- lib/widgets/post_view.dart
+++ lib/widgets/post_view.dart
@@ -1,26 +1,94 @@
+import 'dart:async';
+
+import 'package:animator/animator.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_application/models/post.dart';
 import 'package:instagram_application/models/user.dart';
+import 'package:instagram_application/screens/comments_screen.dart';
 import 'package:instagram_application/screens/profile_screen.dart';
+import 'package:instagram_application/services/database_service.dart';
 
-class PostView extends StatelessWidget {
+class PostView extends StatefulWidget {
   final String currentUserId;
-  final  Post post;
+  final Post post;
   final User author;
 
-  PostView({this.currentUserId,this.post,this.author});
+  PostView({this.currentUserId, this.post, this.author});
+
+  @override
+  _PostViewState createState() => _PostViewState();
+}
+
+class _PostViewState extends State<PostView> {
+  int _likeCount = 0;
+  bool _isLiked = false;
+  bool _heartAnim = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _likeCount = widget.post.likeCount;
+    _initPostLiked();
+  }
+
+  @override
+  void didUpdateWidget(PostView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.post.likeCount != widget.post.likeCount) {
+      _likeCount = widget.post.likeCount;
+    }
+  }
+
+  _initPostLiked() async {
+    bool isLiked = await DatabaseService.didLikePost(
+      currentUserId: widget.currentUserId,
+      post: widget.post,
+    );
+    if (mounted) {
+      setState(() {
+        _isLiked = isLiked;
+      });
+    }
+  }
+
+  _likePost() {
+    if (_isLiked) {
+      //unlike Post
+      DatabaseService.unlikePost(
+          currentUserId: widget.currentUserId, post: widget.post);
+      setState(() {
+        _isLiked = false;
+        _likeCount = _likeCount - 1;
+      });
+    } else {
+      //like post
+      DatabaseService.likePost(
+          currentUserId: widget.currentUserId, post: widget.post);
+      setState(() {
+        _heartAnim = true;
+        _isLiked = true;
+        _likeCount = _likeCount + 1;
+      });
+      Timer(Duration(milliseconds: 350), () {
+        setState(() {
+          _heartAnim = false;
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       children: <Widget>[
         GestureDetector(
           onTap: () => Navigator.push(
             context,
             MaterialPageRoute(
               builder: (BuildContext context) => ProfileScreen(
-                currentUserId: currentUserId,
-                userId: post.authorId,
+                currentUserId: widget.currentUserId,
+                userId: widget.post.authorId,
               ),
             ),
           ),
@@ -31,15 +99,16 @@ class PostView extends StatelessWidget {
                 CircleAvatar(
                   radius: 25,
                   backgroundColor: Colors.grey,
-                  backgroundImage: author.profileImageUrl.isEmpty
+                  backgroundImage: widget.author.profileImageUrl.isEmpty
                       ? AssetImage("assets/images/user_placeholder.jpg")
-                      : CachedNetworkImageProvider(author.profileImageUrl),
+                      : CachedNetworkImageProvider(
+                          widget.author.profileImageUrl),
                 ),
                 SizedBox(
                   width: 10,
                 ),
                 Text(
-                  author.name,
+                  widget.author.name,
                   style: TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
@@ -49,12 +118,35 @@ class PostView extends StatelessWidget {
             ),
           ),
         ),
-        Container(
-          height: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: CachedNetworkImageProvider(post.imageUrl),
-                  fit: BoxFit.cover)),
+        GestureDetector(
+          onDoubleTap: _likePost,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: CachedNetworkImageProvider(widget.post.imageUrl),
+                        fit: BoxFit.cover)),
+              ),
+              _heartAnim
+                  ? Animator(
+                      duration: Duration(microseconds: 300),
+                      tween: Tween(begin: 0.5, end: 1.4),
+                      curve: Curves.elasticOut,
+                      builder: (anim) => Transform.scale(
+                        scale: anim.value,
+                        child: Icon(
+                          Icons.favorite,
+                          size: 100,
+                          color: Colors.red[400],
+                        ),
+                      ),
+                    )
+                  : SizedBox.shrink(),
+            ],
+          ),
         ),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 10),
@@ -64,28 +156,41 @@ class PostView extends StatelessWidget {
               Row(
                 children: <Widget>[
                   IconButton(
-                    icon: Icon(
-                      Icons.favorite_border,
-                      size: 40,
-                    ),
-                    onPressed: () {},
+                    icon: _isLiked
+                        ? Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          )
+                        : Icon(Icons.favorite_border,
+                        color: Colors.grey,),
+                    iconSize: 30,
+                    onPressed: _likePost,
                   ),
                   SizedBox(
                     width: 10,
                   ),
                   IconButton(
                     icon: Icon(
-                      Icons.comment,
-                      size: 40,
+                      Icons.add_comment,
+                      size: 30,
+                      color: Colors.grey,
                     ),
-                    onPressed: () {},
+                    onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CommentsScreen(
+                          postId: widget.post.id,
+                          likeCount: _likeCount,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
                 child: Text(
-                  "1 beğenme",
+                  "${_likeCount.toString()} beğenme",
                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
               ),
@@ -97,14 +202,14 @@ class PostView extends StatelessWidget {
                   Container(
                     margin: EdgeInsets.only(left: 12, right: 6),
                     child: Text(
-                      author.name,
+                      widget.author.name,
                       style:
                           TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
                   ),
                   Expanded(
                     child: Text(
-                      post.caption,
+                      widget.post.caption,
                       style: TextStyle(
                           fontSize: 16,
                           color: Colors.blueGrey,
